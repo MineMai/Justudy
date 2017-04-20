fe71eae1864a0760bc28d74234eece12cbc22166
YenShao <chenyenshao@YenShao-Air.local>
2017-04-21 01:13:03 +0800

    測試一下CollectionCarousel

---
+++ JustStudy/FourthCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  FourthCollectionCell.swift
+//  JustStudy
+//
+//  Created by YenShao on 2017/4/20.
+//  Copyright © 2017年 YenShao. All rights reserved.
+//
+
+import UIKit
+
+class FourthCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var heroImage: UIImageView!
+    
+    
+}
---
+++ JustStudy/FourthVC.swift
@@ -0,0 +1,72 @@
+//
+//  FourthVC.swift
+//  JustStudy
+//
+//  Created by YenShao on 2017/4/20.
+//  Copyright © 2017年 YenShao. All rights reserved.
+//
+
+import UIKit
+
+class FourthVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FourthCollectionCell", for: indexPath) as! FourthCollectionCell
+        
+        cell.heroImage.image = UIImage(named: testdata.posts[indexPath.row]["image"]!)
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
