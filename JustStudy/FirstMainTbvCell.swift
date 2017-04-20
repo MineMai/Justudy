c36a1ce873fbefb202aba67f21ebf3b434ccb1b2
YenShao <chenyenshao@YenShao-Air.local>
2017-04-20 18:24:37 +0800

    刻好FirstMainTbvc

---
+++ JustStudy/FirstMainTbvCell.swift
@@ -0,0 +1,46 @@
+//
+//  FirstMainTbvCell.swift
+//  JustStudy
+//
+//  Created by YenShao on 2017/4/20.
+//  Copyright © 2017年 YenShao. All rights reserved.
+//
+
+import UIKit
+
+class FirstMainTbvCell: UITableViewCell {
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    
+    @IBOutlet weak var firstTopicLabel: UILabel!
+    @IBOutlet weak var firstTimeLabel: UILabel!
+    @IBOutlet weak var firstPlaceLabel: UILabel!
+    @IBOutlet weak var firstLikeBtn: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
