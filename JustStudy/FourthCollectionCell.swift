1754488d65090c851def176705121c5d8d0047be
YenShao <chenyenshao@YenShao-Air.local>
2017-04-25 14:47:14 +0800

    修改達人頁面

--- JustStudy/FourthCollectionCell.swift
+++ JustStudy/FourthCollectionCell.swift
@@ -12,27 +12,33 @@ class FourthCollectionCell: UICollectionViewCell {
     
     @IBOutlet weak var heroImage: UIImageView!
     
-    @IBOutlet weak var heroBackImage: UIImageView!
+    @IBOutlet weak var heroNameLabel: UILabel!
     
-    @IBOutlet weak var effectView: UIVisualEffectView!
+    @IBOutlet weak var heroTopicLabel: UILabel!
+    
+    @IBOutlet weak var heroBackView: UIView!
+    
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         let layer = heroImage?.layer
-        layer?.cornerRadius = 100
+        layer?.cornerRadius = 90
         layer?.masksToBounds = true
         
-        let layer2 = heroBackImage?.layer
-        layer2?.cornerRadius = 20
-        layer2?.masksToBounds = true
+//        let layer2 = heroBackImage?.layer
+//        layer2?.cornerRadius = 20
+//        layer2?.masksToBounds = true
+//        
+//        let layer3 = effectView?.layer
+//        layer3?.cornerRadius = 20
+//        layer3?.masksToBounds = true
+//        layer3?.borderWidth = 1.0
+//        layer3?.borderColor = UIColor.white.cgColor
         
-        let layer3 = effectView?.layer
-        layer3?.cornerRadius = 20
-        layer3?.masksToBounds = true
-        layer3?.borderWidth = 1.0
-        layer3?.borderColor = UIColor.white.cgColor
+        heroBackView.layer.borderWidth = 2.0
+        heroBackView.layer.borderColor = UIColor.white.cgColor
         
         
     }
