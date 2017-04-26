46d40140e3f98bbaa1b388634dce7de5003221fa
YenShao <chenyenshao@YenShao-Air.local>
2017-04-26 16:11:54 +0800

    加入導航

--- JustStudy/FirstDetailTbvc.swift
+++ JustStudy/FirstDetailTbvc.swift
@@ -51,7 +51,7 @@ class FirstDetailTbvc: UITableViewController {
             let cell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath) as! FirstInfoCell
             cell.firstInfoTopic.text = "123"
             cell.firstInfoTime.text = "456"
-            cell.firstInfoPlaceBtn.setTitle("999", for: .normal)
+            cell.firstInfoPlaceBtn.setTitle("10650台北市大安區信義路2段166號", for: .normal)
             return cell
         case 2:
             let cell = tableView.dequeueReusableCell(withIdentifier: "descriptionCell", for: indexPath) as! FirstDescriptionCell
@@ -68,6 +68,32 @@ class FirstDetailTbvc: UITableViewController {
         }
         
     }
+    
+    
+    //MARK: preparForSegue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goMapSegue"
+        {
+            let mapvc = segue.destination as? FirstMapVC
+            mapvc?.targetLocation = "10650台北市大安區信義路2段166號"
+            
+//            if let index = detailindex
+//            {
+//                mapvc?.mapIndex = index
+//            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
@@ -105,49 +131,6 @@ class FirstDetailTbvc: UITableViewController {
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- JustStudy/FirstMapVC.swift
+++ JustStudy/FirstMapVC.swift
@@ -15,7 +15,11 @@ class FirstMapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager() //定位需要用的
+    var myLocation:CLLocationCoordinate2D?    //把位置給導航用的變數
+    var forNavigationLocation:CLLocationCoordinate2D? //把位置給導航用的變數
     var isMoveMap = false
+    
+    var targetLocation:String? //前一頁傳來的地址
     
     @IBAction func mapTypeChange(_ sender: UISegmentedControl)
     {
@@ -43,6 +47,13 @@ class FirstMapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation() //開始執行
+        
+        
+        if let location = targetLocation
+        {
+            print("\(location)")
+            findLocation(address: location)
+        }
         
     }
 
@@ -75,6 +86,104 @@ class FirstMapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
         let region = MKCoordinateRegionMake(currentLocation.coordinate, span)
         mapView.setRegion(region, animated: true)
     }
+    
+    //MARK: 找出目的地
+    func findLocation(address:String)
+    {
+        let geoCoder = CLGeocoder() //地址轉座標
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            
+            if error != nil
+            {
+                print("xx地址轉座標錯誤: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            else
+            {
+                //placemarks裡面傳回來的是可能有好幾個位置，所以用陣列表示
+                if let coordinate = placemarks?[0].location?.coordinate
+                {
+                    self.forNavigationLocation = coordinate // 把位置傳給導航用
+                    let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake((coordinate.latitude), (coordinate.longitude))
+                    let xScale:CLLocationDegrees = 0.01
+                    let yScale:CLLocationDegrees = 0.01
+                    let span:MKCoordinateSpan = MKCoordinateSpanMake(yScale, xScale)
+                    let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+                    self.mapView.setRegion(region, animated: true)
+                    
+                    //加入大頭針
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coordinate
+                    annotation.title = "活動地點"
+                    self.mapView.selectAnnotation(annotation, animated: true) //自動顯示泡泡
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
+    // MARK: - MKMapViewDelegate Methods  自訂泡泡裡面加東西
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        //檢查是否為使用者位置，是的話就不標
+        if annotation is MKUserLocation
+        {
+            return nil
+        }
+        let identifier = "Pin" //大頭針識別號
+        
+        //大頭針回收機制
+        var result = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if result == nil
+        {
+            result = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        
+        result?.canShowCallout = true
+        result?.image = UIImage(named: "pin_red.png")
+        
+        
+        //right callout accessoryview 泡泡右邊助理按鈕
+        let button = UIButton(type: .detailDisclosure)
+        button.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)
+        result?.rightCalloutAccessoryView = button
+        
+        return result
+        
+    }
+    
+    
+    //泡泡裡的按鈕，按下會執行的方法
+    func buttonTapped(sender:UIButton)
+    {
+        let alert = UIAlertController(title: "導航", message: "是否進入導航頁面", preferredStyle: .actionSheet)
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+            
+            let pA = MKPlacemark(coordinate: self.myLocation!, addressDictionary: nil)
+            let pB = MKPlacemark(coordinate: self.forNavigationLocation!, addressDictionary: nil)
+            
+            let miA = MKMapItem(placemark: pA)
+            let miB = MKMapItem(placemark: pB)
+            miA.name = "現在位置"
+            miB.name = "目的地"
+            let routes = [miA, miB]
+            let options = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+            MKMapItem.openMaps(with: routes, launchOptions: options)
+            
+        }
+        let cancle = UIAlertAction(title: "取消", style: .destructive, handler: nil)
+        alert.addAction(ok)
+        alert.addAction(cancle)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    
+    
     
 
 }
